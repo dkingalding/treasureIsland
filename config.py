c1357a16d65e90ce23e2420400cf5301dd77a19e
alading <2502309334@qq.com>
2019-12-29 18:48:10 +0800

    del loggit

--- config.py
+++
@@ -1,9 +0,0 @@
-Agent = [
-    {"User-Agent":"Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50"},
-    {"User-Agent":"Mozilla/5.0 (Windows; U; Windows NT 6.1; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50"},
-    {"User-Agent":"Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0"},
-    {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11"}
-]
-mymysql = {"host":'120.27.22.37', "user":"root", "passwd":"Af2Jna8Fs91p","db":'duobaodao'  }
-
-myredis = {"host":'120.27.22.37', "port":6347,}
