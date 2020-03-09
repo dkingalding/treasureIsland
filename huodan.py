98b3956f2cdb35043ca2b6762cfb37854673000f
alading <2502309334@qq.com>
2020-03-09 15:18:09 +0800

    修该库存

--- huodan.py
+++ huodan.py
@@ -51,8 +51,8 @@ class huodan(object):
         # sql = "SELECT  a.usedNo, a.quality, a.cappedPrice, b.productName ,c.vagePrice FROM goods as a JOIN usedName as b ON b.usedNo = a.usedNo" \
         #       " JOIN theprice as c ON c.usedNo = a.usedNo WHERE  {1} AND a.endTime >= {2} AND {0}  GROUP BY a.usedNo".format(condition, cond, auconttime)
 
-        sql = "SELECT  a.usedNo, a.quality, a.cappedPrice, b.productName ,c.vagePrice , c.notes FROM goods as a JOIN usedName as b ON b.usedNo = a.usedNo" \
-              " JOIN theprice as c ON c.usedNo = a.usedNo WHERE  {1}  AND {0}  GROUP BY a.usedNo  ORDER BY  b.productName ".format(condition, cond, auconttime)
+        sql = "SELECT  a.usedNo, a.quality, a.cappedPrice, b.productName ,c.vagePrice, c.notes FROM goods as a JOIN usedName as b ON b.usedNo = a.usedNo" \
+              " JOIN theprice as c ON c.usedNo = a.usedNo WHERE  {1} AND a.endTime >= {2} AND {0}  GROUP BY a.usedNo  ORDER BY  b.productName ".format(condition, cond, auconttime)
 
         self.cursor.execute(sql)
 
@@ -76,7 +76,7 @@ class huodan(object):
                     myprice = round(goods[2] *0.85)
                 if myprice <= ableprice:
                     content = content + "\r\n" + goods[3] + '----' + goods[1] + '----原价' + str(
-                        goods[2]) + '----包邮价' + str(myprice)+ '----'+goods[5]+"\r\n"
+                        goods[2]) + '----包邮价' + str(myprice)+ '----' + goods[5] +"\r\n"
                 else:
                     print(goods, ableprice, myprice)
                 self.redislink.sadd('kuchun', goods[0])
@@ -93,10 +93,10 @@ class huodan(object):
             mailcontent = ''
             self.redislink.delete('kuchun')
             self.redislink.sadd('kuchun', 0)
-            for keyword in keywordlist:
-                onecontent = self.getlist(keyword[0])
-                # print(onecontent)
-                mailcontent = mailcontent+ "\r\n" + keyword[0] + "===库存列表" + "\r\n" + onecontent
+            # for keyword in keywordlist:
+            onecontent = self.getlist('')
+            #     # print(onecontent)
+            mailcontent = mailcontent+ "\r\n" + "===库存列表" + "\r\n" + onecontent
     
             titl = '库存清单'
             mailclass = dingmail()
