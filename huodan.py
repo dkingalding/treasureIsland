de855bc16c340f9425ec2d8573f9d88ae870bac2
alading <2502309334@qq.com>
2020-03-31 02:22:33 +0800

    kucun

--- huodan.py
+++ huodan.py
@@ -63,14 +63,14 @@ class huodan(object):
         #       " JOIN theprice as c ON c.usedNo = a.usedNo WHERE  {1} AND a.endTime >= {2} AND {0}  GROUP BY a.usedNo".format(condition, cond, auconttime)
 
         sql = "SELECT  a.usedNo, a.quality, a.cappedPrice, b.productName ,c.vagePrice, c.notes FROM goods as a LEFT JOIN usedName as b ON b.usedNo = a.usedNo" \
-              " LEFT JOIN theprice as c ON c.usedNo = a.usedNo WHERE  {1} AND a.endTime >= {2}   GROUP BY a.usedNo  ORDER BY  a.usedNo ".format(condition, cond, auconttime)
+              " LEFT JOIN theprice as c ON c.usedNo = a.usedNo WHERE  {1} AND a.endTime >= {2} AND a.cappedPrice > 100  GROUP BY a.usedNo  ORDER BY  a.endTime DESC ".format(condition, cond, auconttime)
 
         self.cursor.execute(sql)
 
         self.myqllink.commit()
         results = self.cursor.fetchall()
         content = ''
-        content2 = ''
+
         for goods in results:
             #如果有价格记录
             if self.redislink.sismember('kuchun', goods[0]) == False:
@@ -104,13 +104,13 @@ class huodan(object):
                     # print(goods[3] ,goods[1],cappedPrice,myprice,goods[5])
                     # print("\r\n" + goods[3] + '----' + goods[1] + '----原价' + str(cappedPrice) + '----包邮价' + str(myprice) + '----' + goods[5] + "\r\n")
                     content = content + "\r\n" + goods[3] + '----' + goods[1] + '----原价' + str(cappedPrice) + '----包邮价' + str(myprice) + '--/--' + notes +"\r\n"
-                    content2 = content2 + "\r\n" + goods[3] + '----' + goods[1] + '----原价' + str(cappedPrice) + "\r\n"
+
                 else:
                     print(goods, ableprice, myprice)
                 self.redislink.sadd('kuchun', goods[0])
 
         # print(content)
-        return content,content2
+        return content
         # if content:
 
 
@@ -120,7 +120,7 @@ class huodan(object):
             keywordlist = self.getwords()
             # print(keywordlist)
             mailcontent = ''
-            mailcontent2 = ''
+
             self.redislink.delete('kuchun')
             self.redislink.sadd('kuchun', 0)
             # onecontent = self.getlist('')
@@ -136,15 +136,13 @@ class huodan(object):
                     kucuntitl = keyword[0]
                 else:
                     kucuntitl ="其他"
-                mailcontent = mailcontent+ "\r\n" + str(kucuntitl) + "===库存列表" + "\r\n" + onecontent[0]
-                mailcontent2 = mailcontent2 + "\r\n" + str(kucuntitl) + "===库存列表" + "\r\n" + onecontent[1]
+                mailcontent = mailcontent+ "\r\n" + str(kucuntitl) + "===库存列表" + "\r\n" + onecontent
+
     
             titl = '库存清单'
             mailclass = dingmail()
             mailclass.sendmail(titl, mailcontent)
 
-            titl2 = '客户库存清单'
-            mailclass.sendmail(titl2, mailcontent)
             self.redislink.delete('kuchun')
 
 
