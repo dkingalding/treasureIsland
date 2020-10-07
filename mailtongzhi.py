781fbb8e23ee43c871581be5d711591d95a3dd00
alading <2502309334@qq.com>
2020-10-07 21:04:57 +0800

    add yinzhaowei mail

--- mailtongzhi.py
+++ mailtongzhi.py
@@ -5,7 +5,7 @@ from email.header import Header
 class dingmail(object):
     def __init__(self):
         self.from_addr='2502309334@qq.com'   #邮件发送账号
-        self.to_addrs='1247975688@qq.com'   #接收邮件账号
+        self.to_addrs=['1247975688@qq.com','yingzhaowei3512@dingtalk.com']   #接收邮件账号
         self.qqCode='fmfrjdqrgmsnecbf'   #授权码（这个要填自己获取到的）
         self.smtp_server='smtp.qq.com'#固定写死
         self.smtp_port=465#固定端口
@@ -23,9 +23,9 @@ class dingmail(object):
         message['To'] = Header("", 'utf-8')   #收件人
         subject = titl
         message['Subject'] = Header(subject, 'utf-8')  #邮件标题
-
-        try:
-            stmp.sendmail(self.from_addr, self.to_addrs, message.as_string())
-        except Exception as e:
-            print ('邮件发送失败--' + str(e))
-        print ('邮件发送成功')
+        for mail in  self.to_addrs:
+            try:
+                stmp.sendmail(self.from_addr, mail, message.as_string())
+            except Exception as e:
+                print('邮件发送失败--' + str(e))
+            print('邮件发送成功')
